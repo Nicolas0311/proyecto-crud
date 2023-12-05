3ae20aecd7ff1cc6038f3796f4b5acd1926ba8d4
Nicolas0311 <nicolas1994sanchez@gmail.com>
2023-12-05 00:23:56 -0300

    Se completa el crud

--- modulos/contactos/edit.php
+++ modulos/contactos/edit.php
@@ -5,8 +5,7 @@ include("../../conexion.php");
 if(isset($_GET['id'])){
 
     $txtid=(isset($_GET['id'])?$_GET['id']:"");
-    $stm=$conexion->prepare("SELECT * FROM contactos WHERE id=:txtid");
-    $stm->bindParam(":txtid,$txtid");
+    $stm=$conexion->prepare("SELECT * FROM contactos WHERE id=$txtid");
     $stm->execute();
     $registro=$stm->fetch(PDO::FETCH_LAZY);
     $nombre=$registro['nombre'];
@@ -15,8 +14,27 @@ if(isset($_GET['id'])){
 
 
 }
-    
 
+if($_POST){
+   
+
+
+    $txtid=(isset($_POST['txtid'])?$_POST['txtid']:"");
+    $nombre=(isset($_POST['nombre'])?$_POST['nombre']:"");
+    var_dump($txtid);
+    $fecha=(isset($_POST['fecha'])?$_POST['fecha']:"");
+    $telefono=(isset($_POST['telefono'])?$_POST['telefono']:"");
+
+    $stm=$conexion->prepare("UPDATE contactos SET nombre=:nombre,telefono=:telefono,fecha=:fecha WHERE id=:txtid"); 
+    $stm->bindParam(':nombre',$nombre );
+    $stm->bindParam(':telefono',$telefono );
+    $stm->bindParam(':fecha',$fecha );
+    $stm->bindParam(':txtid',$txtid );
+    $stm->execute();
+  
+    header("location:index.php");
+    
+}
 ?>
 
 <?php include("../../templates/header.php"); ?>
